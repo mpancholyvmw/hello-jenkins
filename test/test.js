e5a010f1dca4f8c897d72cf86c4ad483ffae134a
mpancholyvmw <mpancholy@vmare.com>
2016-05-19 17:50:26 -0500

    Testing the git server committ from hello-jenkins.

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var app = require('../app.js');
 
 describe('GET /',function(){
 	it('responded with hello world',function(done){
-		request(app).get('/').expect('hello world',done);
+		request(app).get('/').expect('hello Tom',done);
 	});
 });
 
